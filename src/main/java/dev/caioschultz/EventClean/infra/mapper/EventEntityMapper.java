4e8822d841a350383a8947575a44ecad6b8fec5d
Caio Mazziero Schultz <caio.mazziero.schultz@gmail.com>
2025-09-15 16:08:30 -0300

    feat: cria mapper para transformar domain em entidade e vice-versa

---
+++ src/main/java/dev/caioschultz/EventClean/infra/mapper/EventEntityMapper.java
@@ -0,0 +1,43 @@
+package dev.caioschultz.EventClean.infra.mapper;
+
+import dev.caioschultz.EventClean.core.entities.Event;
+import dev.caioschultz.EventClean.infra.persistence.EventEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EventEntityMapper {
+
+
+    public EventEntity toEntity(Event event){
+
+        return new EventEntity(event.id(),
+                              event.name(),
+                              event.description(),
+                              event.identifier(),
+                              event.startDate(),
+                              event.endDate(),
+                              event.address(),
+                              event.organizer(),
+                              event.capacity(),
+                              event.eventType()
+        );
+    }
+
+    public Event toDomain(EventEntity eventEntity){
+
+        return new Event(eventEntity.getId(),
+                         eventEntity.getName(),
+                         eventEntity.getDescription(),
+                         eventEntity.getIdentifier(),
+                         eventEntity.getStartDate(),
+                         eventEntity.getEndDate(),
+                         eventEntity.getAddress(),
+                         eventEntity.getOrganizer(),
+                         eventEntity.getCapacity(),
+                         eventEntity.getEventType()
+        );
+    }
+
+
+
+}

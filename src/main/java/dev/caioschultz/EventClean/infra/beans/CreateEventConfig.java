e6efd3e6790f5ee39c9628e8e07427da00999e4e
Caio Mazziero Schultz <caio.mazziero.schultz@gmail.com>
2025-09-16 14:06:39 -0300

    docs: faz comentários com algumas informações na classe CreateEventConfig

--- src/main/java/dev/caioschultz/EventClean/infra/beans/CreateEventConfig.java
+++ src/main/java/dev/caioschultz/EventClean/infra/beans/CreateEventConfig.java
@@ -1,4 +1,4 @@
-package dev.caioschultz.EventClean.config;
+package dev.caioschultz.EventClean.infra.beans;
 
 import dev.caioschultz.EventClean.core.usecases.create.CreateEventCaseImpl;
 import dev.caioschultz.EventClean.infra.gateway.EventRepositoryGateway;
@@ -12,4 +12,10 @@ public class CreateEventConfig {
     public CreateEventCaseImpl createEventCase(EventRepositoryGateway gateway){
         return new CreateEventCaseImpl(gateway);
     }
+
+    // Cria-se uma classe de config para dar contexto ao Spring
+    // A classe CreateEventCaseImpl é como se fosse um @Service
+    // No entanto, se queremos nos manter na arquitetura limpa não podemos colocar a anotation @Service
+    // Apesar de que funcionaria normalmente se fizessemos isso
+    // Dessa forma, fazemos uma classe de Config usando a anotation @Bean para dar contexto ao Spring
 }

64a46ae8bc9683fed025dfaca60b4ba2a1ff8ae0
Caio Mazziero Schultz <caio.mazziero.schultz@gmail.com>
2025-09-17 16:46:09 -0300

    feat: adiciona resposta customizadas no controller usando ResponseEntity

--- src/main/java/dev/caioschultz/EventClean/infra/presentation/EventController.java
+++ src/main/java/dev/caioschultz/EventClean/infra/presentation/EventController.java
@@ -5,9 +5,13 @@ import dev.caioschultz.EventClean.core.usecases.findall.FindAllEventUseCase;
 import dev.caioschultz.EventClean.infra.dtos.EventDto;
 import dev.caioschultz.EventClean.infra.mapper.EventDtoMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/eventclean")
@@ -19,22 +23,29 @@ public class EventController {
     private final FindAllEventUseCase findAllEventCase;
 
 
+    // Retornando com Map o ResponseEntity é possível retornar uma mensagem + um objeto (no caso, o EventoDto criado)
     @PostMapping("/create")
-    public EventDto createEvent(@RequestBody EventDto request){
+    public ResponseEntity<Map<String, Object>> createEvent(@RequestBody EventDto request){
 
         Event newEvent = createEventCase.execute(eventMapper.toDomain(request));
+        EventDto createdEvent = eventMapper.toDto(newEvent);
 
-        return eventMapper.toDto(newEvent);
+        Map<String, Object> response = new HashMap<>();
+        response.put("Mensagem: " , "Evento salvo com succeso!");
+        response.put("Dados do evento: " , createdEvent);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(response);
     }
 
     @GetMapping
-    public List<EventDto> findAllEvents(){
+    public ResponseEntity<List<EventDto>> findAllEvents(){
 
         List<Event> events = findAllEventCase.execute();
 
-        return events.stream()
+        return ResponseEntity.ok(events.stream()
                 .map(eventMapper::toDto)
-                .toList();
+                .toList());
     }
 
 }

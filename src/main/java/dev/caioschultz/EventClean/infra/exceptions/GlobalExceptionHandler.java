0f292e71329cf3ee821ce16c10b0678895072158
Caio Mazziero Schultz <caio.mazziero.schultz@gmail.com>
2025-09-19 14:31:09 -0300

    refactor: customiza o retorno da exception UniqueConstraintException

--- src/main/java/dev/caioschultz/EventClean/infra/exceptions/GlobalExceptionHandler.java
+++ src/main/java/dev/caioschultz/EventClean/infra/exceptions/GlobalExceptionHandler.java
@@ -5,13 +5,21 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(UniqueConstraintViolationException.class)
-    public ResponseEntity<String> handleUniqueConstraintViolationException(UniqueConstraintViolationException exception){
+    public ResponseEntity<Map<String, String>> handleUniqueConstraintViolationException(UniqueConstraintViolationException exception){
+        Map<String, String> response = new HashMap<>();
+
+        response.put("Error: ", exception.getMessage());
+        response.put("Message: ", "Please, insert a valid hashId for you event!");
+
         return ResponseEntity.status(HttpStatus.CONFLICT)
-                .body(exception.getMessage());
+                .body(response);
     }
 
 }
